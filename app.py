6ac2d176dc375cff8b6ba24b2661a0bd30e11788
Vinícius de Andrade Pereira <viniandrade235@gmail.com>
2019-03-04 02:48:00 -0300

    Button update, structures improved.

--- app.py
+++ app.py
@@ -3,18 +3,17 @@ import tkinter as tk
 class ExampleApp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        self.label = tk.Label(self, text="", width=50)
-        self.button = tk.Button(self, text="Count", width = 10)
+        self.label = tk.Label(self, text="Tempo restante para se tornar mestre: 00:00:00", width=50)
+        self.button = tk.Button(self, text="Count", command=self.countdown, width=10)
+        '''self.button1 = tk.Button(self, text="Save", command=self.save, width=10)'''
         self.label.pack()
         self.button.pack()
+        '''self.button1.pack()'''
         self.s = 0
         self.m = 0
         self.h = 0
-        self.countdown(0)
 
-    def countdown(self, remaining = None):
-        if remaining is not None:
-            self.s = remaining
+    def countdown(self):
         if self.s == 59:
             self.label.configure(text="{}:{}:{}".format(self.h, self.m, self.s))
             self.m = self.m + 1
@@ -26,10 +25,14 @@ class ExampleApp(tk.Tk):
             self.m = 0
             self.after(1000, self.countdown)
         else:
-            self.label.configure(text="Tempo restante para se tornar mestre: {}:{}:{}".format(self.h, self.m, self.s))
+            self.label.configure(text="Tempo restante para se tornar mestre: 0{}:0{}:0{}"
+                                 .format(self.h, self.m, self.s))
+            if self.s >= 10:
+                self.label.configure(text="Tempo restante para se tornar mestre: 0{}:0{}:{}"
+                                     .format(self.h, self.m, self.s))
             self.s = self.s + 1
             self.after(1000, self.countdown)
-
+    '''def save (self):'''
 
 if __name__ == "__main__":
     app = ExampleApp()
